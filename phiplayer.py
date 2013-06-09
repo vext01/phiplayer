e5b4f412afbfe41cf5a77d0b7bc48205878d0bca
vext01 <vext01@gmail.com>
2013-06-09 22:28:09 +0100

    populate menu

--- phiplayer.py
+++ phiplayer.py
@@ -5,16 +5,17 @@ import broadcast
 GET_IPLAYER="get_iplayer"
 class PhiPlayerError(Exception): pass
 
-def item_chosen(button, choice):
-    print("Chose: %s" % choice)
+def item_chosen(button):
+    print(button.label)
 
-
+# XXX make menu items prettier
 def mk_menu(title, items):
     body = [urwid.Text(title), urwid.Divider()]
 
     for c in items:
-        button = urwid.Button(c)
-        urwid.connect_signal(button, 'click', item_chosen, c)
+        button = urwid.Button(c, on_press=item_chosen)
+        button._w = urwid.AttrMap(urwid.SelectableIcon(c, 1),
+                None, focus_map='reversed')
         body.append(urwid.AttrMap(button, None, focus_map='reversed'))
 
     return urwid.ListBox(urwid.SimpleFocusListWalker(body))
@@ -35,19 +36,26 @@ def get_broadcast_list():
         if not re.match("^[0-9]*:", line): continue
         bcasts.append(broadcast.Broadcast.from_line(line))
 
-    for i in bcasts:
-        print(i)
+    return bcasts
+
+def broadcast_menu_str(bcast):
+    cats = ", ".join(bcast.categs)
+    return "%s: %s - %s. %s" % \
+            (bcast.pid, bcast.title, bcast.descr, cats)
 
 if __name__ == "__main__":
-    get_broadcast_list()
+    bcasts = get_broadcast_list()
 
+    if not bcasts: raise PhiPlayerError("Could not find any broadcasts")
 
-    """
-    items = [ "test%s" % str(x) for x in range(10) ]
-    main = urwid.Padding(mk_menu("THIS IS A TEST", items), left=2, right=2)
+    # build a simple menu
+    items = [ broadcast_menu_str(x) for x in bcasts ]
+    main = urwid.Padding(mk_menu("Broadcasts", items), left=0, right=0)
+
+    # build the toplevel ui
     top = urwid.Overlay(main, urwid.SolidFill(u'\N{MEDIUM SHADE}'),
-            align='center', width=('relative', 60),
-            valign='middle', height=('relative', 60),
-            min_width=20, min_height=9)
+            align='center', width=('relative', 100),
+            valign='middle', height=('relative', 100))
+
+    # let's go
     urwid.MainLoop(top, palette=[('reversed', 'standout', '')]).run()
-    """
